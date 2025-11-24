ce6dc3e6caf7d179a7381a1d6808be6b1e62cc16
Jikuhara <kei2003730@gmail.com>
2025-11-24 22:19:44 +0900

    test: pthread join 2nd variable rtv

--- sandbox_pthread/correct_test.c
+++ sandbox_pthread/correct_test.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 18:53:14 by kjikuhar          #+#    #+#             */
-/*   Updated: 2025/11/24 19:08:17 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2025/11/24 22:18:50 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int				count = 0;
 pthread_mutex_t	mutex;
+void			*pointer = (void *)0x10;
 
 void	*routine(void *p)
 {
@@ -28,20 +29,24 @@ void	*routine(void *p)
 		pthread_mutex_unlock(&mutex);
 		i++;
 	}
-	return (NULL);
+	return (pointer++);
 }
 
 int main(int argc, char const *argv[])
 {
 	(void)argv, (void)argv;
 	pthread_t	p1, p2;
+	void		*rtv_pointer;
 
 	pthread_mutex_init(&mutex, NULL);
 	pthread_create(&p1, NULL, *routine, NULL);
 	pthread_create(&p2, NULL, *routine, NULL);
 
-	pthread_join(p1, NULL);
-	pthread_join(p2, NULL);
+	rtv_pointer = NULL;
+	pthread_join(p1, &rtv_pointer);
+	printf("rtv_pointer = %p\n", rtv_pointer);
+	pthread_join(p2, &rtv_pointer);
+	printf("rtv_pointer = %p\n", rtv_pointer);
 
 	pthread_mutex_destroy(&mutex);
 	printf("count = %d\n", count);
