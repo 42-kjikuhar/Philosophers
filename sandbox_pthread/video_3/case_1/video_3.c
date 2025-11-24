24699c1b82993d6b9de6def7a15a5ea9a443c664
Jikuhara <kei2003730@gmail.com>
2025-11-24 22:28:05 +0900

    finish: watching and implement the video 3

--- sandbox_pthread/video_3/case_1/video_3.c
+++ sandbox_pthread/video_3/case_1/video_3.c
@@ -3,33 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   video_3.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 14:42:50 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/11 15:27:43 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/11/24 22:26:44 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int mails = 0;
-void *routine() {
-	for (int i = 0; i < 10000000; i++) {
+int		mails = 0;
+void	*routine(void *ptr)
+{
+	(void)ptr;
+	for (int i = 0; i < 10000000; i++)
+	{
 		mails++;
 		// read mails
 		// increment
 		// write mails
 	}
+	return (NULL);
 }
 
-int main(void) {
+int	main(void)
+{
 	pthread_t t1, t2;
-	if (pthread_create(&t1, NULL, routine, NULL) != 0)
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
 		return (1);
-	if (pthread_create(&t2, NULL, routine, NULL) != 0)
+	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
 		return (2);
 	if (pthread_join(t1, NULL) != 0)
 		return (3);
