830df242151c61194aeab4cc14835fb918efba66
jiku0730 <github@jiku.jp>
2026-04-23 03:22:06 +0900

    enjoy: プラトン君を追加してみた。

--- philo/philo_sandbox.c
+++ philo/philo_sandbox.c
@@ -1,18 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   philo_sandbox.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/23 03:17:46 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/04/23 03:17:47 by kjikuhar         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <pthread.h>
 
+void	*plato_routine(void *fork)
+{
+	printf("Plato is thinking.\n");
+	pthread_mutex_lock(fork);
+	printf("Plato gets a fork.\n");
+	pthread_mutex_unlock(fork);
+	printf("Plato leaves a fork.\n");
+	return (NULL);
+}
 void	*socrates_routine(void *fork)
 {
 	printf("Socrates is thinking.\n");
@@ -26,11 +23,14 @@ void	*socrates_routine(void *fork)
 int	main(void)
 {
 	pthread_t		socrates;
+	pthread_t		plato;
 	pthread_mutex_t	fork;
 
 	pthread_mutex_init(&fork, NULL);
 	pthread_create(&socrates, NULL, socrates_routine, &fork);
+	pthread_create(&plato, NULL, plato_routine, &fork);
 	pthread_join(socrates, NULL);
+	pthread_join(plato, NULL);
 	pthread_mutex_destroy(&fork);
 	return (0);
 }
