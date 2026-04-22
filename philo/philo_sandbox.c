d183d6b64862f5dca32256ab6ca624d9d607c963
jiku0730 <github@jiku.jp>
2026-04-23 03:16:57 +0900

    【検証】ソクラテスに、Fork持たせてみた。【ドッキリ】

--- philo/philo_sandbox.c
+++ philo/philo_sandbox.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void	*socrates_routine(void *arg)
+void	*socrates_routine(void *fork)
 {
-	(void)arg;
 	printf("Socrates is thinking.\n");
+	pthread_mutex_lock(fork);
+	printf("Socrates gets a fork.\n");
+	pthread_mutex_unlock(fork);
+	printf("Socrates leaves a fork.\n");
 	return (NULL);
 }
 
 int	main(void)
 {
-	pthread_t	socrates;
+	pthread_t		socrates;
+	pthread_mutex_t	fork;
 
-	pthread_create(&socrates, NULL, socrates_routine, NULL);
+	pthread_mutex_init(&fork, NULL);
+	pthread_create(&socrates, NULL, socrates_routine, &fork);
 	pthread_join(socrates, NULL);
+	pthread_mutex_destroy(&fork);
 	return (0);
 }
