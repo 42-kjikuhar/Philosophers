7fbb1e00840be593778695bdd6c3067d03cebdea
jiku0730 <github@jiku.jp>
2026-04-23 02:16:28 +0900

    fix: t_philo_infoを、ポインタ宣言から、実態のある宣言へ変更。

--- philo/main.c
+++ philo/main.c
@@ -6,18 +6,18 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 01:36:25 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/04/23 02:07:29 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/04/23 02:15:42 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	free_all(t_philo_info *info, pthread_t *philos, pthread_mutex_t *forks)
+int	free_all(int philo_num, pthread_t *philos, pthread_mutex_t *forks)
 {
 	int	i;
 
 	i = 0;
-	while (i < info->philo_num)
+	while (i < philo_num)
 	{
 		if (philos[i])
 		{
@@ -36,12 +36,12 @@ int	free_all(t_philo_info *info, pthread_t *philos, pthread_mutex_t *forks)
 	return (0);
 }
 
-int	prepare_forks(t_philo_info *info, pthread_mutex_t *forks)
+int	prepare_forks(int philo_num, pthread_mutex_t *forks)
 {
 	int	i;
 
 	i = 0;
-	while (i < info->philo_num)
+	while (i < philo_num)
 	{
 		if (pthread_mutex_init(&forks[i], NULL) != 0)
 		{
@@ -56,12 +56,12 @@ int	test(void *arg)
 {
 }
 
-int	prepare_philos(t_philo_info *info, pthread_t *philos)
+int	prepare_philos(int philo_num, pthread_t *philos)
 {
 	int	i;
 
 	i = 0;
-	while (i < info->philo_num)
+	while (i < philo_num)
 	{
 		if (pthread_create(&philos[i], NULL, test, NULL) == 0)
 		{
@@ -74,7 +74,7 @@ int	prepare_philos(t_philo_info *info, pthread_t *philos)
 
 int	main(int argc, char const *argv[])
 {
-	t_philo_info	*info;
+	t_philo_info	info;
 	pthread_t		*philos;
 	pthread_mutex_t	*forks;
 
@@ -83,19 +83,19 @@ int	main(int argc, char const *argv[])
 		printf("Your input is wrong.\n");
 		return (1);
 	}
-	info->philo_num = atoi(argv[1]);
-	info->time_to_die_ms = atoi(argv[2]);
-	info->time_to_eat_ms = atoi(argv[3]);
-	info->time_to_sleep_ms = atoi(argv[4]);
+	info.philo_num = atoi(argv[1]);
+	info.time_to_die_ms = atoi(argv[2]);
+	info.time_to_eat_ms = atoi(argv[3]);
+	info.time_to_sleep_ms = atoi(argv[4]);
 	if (argc == 6)
-		info->number_of_times_each_philosopher_must_eat = atoi(argv[5]);
-	philos = malloc(sizeof(pthread_t) * info->philo_num);
-	forks = malloc(sizeof(pthread_mutex_t) * info->philo_num);
+		info.number_of_times_each_philosopher_must_eat = atoi(argv[5]);
+	philos = malloc(sizeof(pthread_t) * info.philo_num);
+	forks = malloc(sizeof(pthread_mutex_t) * info.philo_num);
 	if (!philos || !forks)
-		return (free_all(info, philos, forks));
-	prepare_forks(info, forks);
-	prepare_philos(info, philos);
+		return (free_all(info.philo_num, philos, forks));
+	prepare_forks(info.philo_num, forks);
+	prepare_philos(info.philo_num, philos);
 	printf("Hello World!");
-	free_all(info, philos, forks);
+	free_all(info.philo_num, philos, forks);
 	return (0);
 }
